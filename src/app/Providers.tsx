47ddb0aa98dd3fd1d328184db9c6ccc38b766cbf
Md Tahmid Alam <mdtahmidalam122@gmail.com>
2026-08-15 14:01:55 +0600

    feat: add Providers component with nextjs-toploader and Redux Persist integration

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -3,10 +3,17 @@ import React, { ReactNode } from "react";
 import { persistor, store } from "@/store/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
+import NextTopLoader from "nextjs-toploader";
 
 const Providers = ({ children }: { children: ReactNode }) => {
   return (
     <div>
+      <NextTopLoader
+        color="#abed33"
+        showSpinner={false}
+        crawlSpeed={2000}
+        easing="ease"
+      />
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           {children}
